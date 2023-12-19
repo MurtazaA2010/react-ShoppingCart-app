53124bd8ef0e92187d2fc9c9c242074dff972c27
Murtaza_Abd <140527812+MurtazaA2010@users.noreply.github.com>
2023-12-19 20:42:52 +0600

    Add files via upload

---
+++ src/Components/laptop.jsx
@@ -0,0 +1,26 @@
+import './css/hero.css'
+import Products from '../JSON/Products.json' 
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+    const Laptop = ({addToCart}) => {
+        const filteredProducts = Products.filter((product) => product.Categorie === 'Laptop');
+    return (
+        <div className="hero">
+            <div className="laptops">
+                <div className="cards">
+                    {filteredProducts.map((product) => (
+                        <div className="card" key={product.id}>
+                            <Link to={`/products/${product.id}`} >
+                                <img src={product.src1} alt="" className='card-img'/>
+                                <p >{product.title}</p>  
+                                <p className='price'>${product['new-price']}<br/><s>${product['old-price']}</s></p> 
+                            </Link>                    
+                            <button className='c-btn' onClick={()=> addToCart(product)}>Add To Cart</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div> 
+    );
+}
+ 
+export default Laptop;
